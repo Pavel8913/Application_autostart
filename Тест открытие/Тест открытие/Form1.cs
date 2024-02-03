65520a2093ac42272d14bfa9d322556013fc48d2
Pavel <143266883+Pavel8913@users.noreply.github.com>
2024-02-03 14:55:17 +0700

    Add files via upload
    This application is added to startup and can also open applications

---
+++ Тест открытие/Тест открытие/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Тест_открытие
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("devenv.exe");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            auto_run(true,Assembly.GetExecutingAssembly().Location);
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome.exe");
+        }
+        
+        private bool auto_run(bool auto_Run,string path)
+        {
+            const string name = "Автозапуск";
+
+            string Exepath = path;
+
+            RegistryKey reg;
+
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+
+
+                if (auto_Run)
+                {
+                    reg.SetValue(name, Exepath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+                reg.Flush();
+                reg.Close();
+            } 
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
